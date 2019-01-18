af447f205c3bf47b5c0871b67227493f2c55c218
unknown <traff1997@gmail.com>
2019-01-18 19:26:46 +0100

    fixed not adding duplicate headers

--- nationen_parser.py
+++ nationen_parser.py
@@ -1,31 +1,40 @@
 from bs4 import BeautifulSoup
 import re
 
+header_file = "nationen_headers.txt"
+
 # Super inefficient way to add new title to file
 # todo: find a better way to do this
-def add_header(header, file):
+def add_header(header, filepath):
 
+    file = open(filepath, "r")
     lines = file.readlines()
     for line in lines:
+        line = re.sub("[\n]", "", line)
         # Skip if the title already exists
         if line == header:
             return 1
     
+    file = open(filepath, "a")
     file.write(header + "\n")
     return 0
 
-# Read html file (right now just as file)
-raw_file = open("nationen.html", "r")
-header_file = open("nationen_headers.txt", "a+")
-soup = BeautifulSoup(raw_file, "lxml")
+def parse_eb_file(filepath):
 
-# Find all oo-line class tags
-tags = soup.findAll("div", class_="oo-line")
+    # Read html file (right now just as file)
+    raw_file = open(filepath, "r")
+    soup = BeautifulSoup(raw_file, "lxml")
 
-for tag in tags:
-    # Skip invalid lines
-    if tag.string == None:
-        continue
-    s = tag.string
-    s = re.sub("[\t\n]", "", s).lstrip(' ')
-    add_header(s, header_file)
+    # Find all oo-line class tags
+    tags = soup.findAll("div", class_="oo-line")
+
+    for tag in tags:
+        # Skip invalid lines
+        if tag.string == None:
+            continue
+        s = tag.string
+        s = re.sub("[\t\n]", "", s).lstrip(' ')
+        add_header(s, header_file)
+
+parse_eb_file("1849.html")
+parse_eb_file("nationen.html")
