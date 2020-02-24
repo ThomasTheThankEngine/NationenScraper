f36397e19b0dc3b12ed70f110abaf93fc7e970e5
Traffalafel <traff1997@gmail.com>
2020-02-24 13:38:04 +0100

    fixed problem with writelines not inserting newline chars

--- nationen_scraper.py
+++ nationen_scraper.py
@@ -5,17 +5,16 @@ import re
 def fetch_html(url):
     http = urllib3.PoolManager()
     response = http.request('GET', url)
-    return response.data.decode('cp1252')
+    # raw = response.data.decode('cp1252')
+    raw = response.data.decode('utf-8')
+    return raw
 
 
 def parse_eb_html(raw_html):
-
     soup = BeautifulSoup(raw_html, "lxml")
     titles = []
-
     # Apparently all tags called 'oo-line' contain
     tags = soup.findAll("div", class_="oo-line")
-
     for tag in tags:
         # Skip invalid lines
         if tag.string == None:
@@ -24,26 +23,24 @@ def parse_eb_html(raw_html):
         title = re.sub("[\t\n]", "", title)
         title = title.lstrip(' ')
         titles.append(title)
-    
     # Remove duplicates 
     titles = list(set(titles))
-
     return titles
 
 
 def append_titles(titles, filepath):
-    
     existing_titles = set()
     try:
-        with open(filepath, 'r+') as file:
+        with open(filepath, 'r+', encoding='utf-8') as file:
             for title in file.readlines():
-                existing_titles.add(title)
+                existing_titles.add(title.rstrip('\n'))
     except:
         print("Error trying to read from file %s, skipping this step" % (filepath))
-    
-    new_titles = set(titles).difference(existing_titles)
-    with open(filepath, 'a+') as file:
-        file.writelines(new_title + '\n' for new_title in new_titles)
+    new_titles = (set(titles)).difference(existing_titles)
+    print('Existing titles: %i' % (len(existing_titles)))
+    print('New titles to add: %i' % (len(new_titles)))
+    with open(filepath, 'a', encoding='utf-8') as file:
+        file.writelines([new_title + '\n' for new_title in new_titles])
 
 
 def scrape_to_file(filepath):
@@ -51,7 +48,6 @@ def scrape_to_file(filepath):
     titles_nationen = parse_eb_html(raw_nationen)
     raw_1849 = fetch_html('https://ekstrabladet.dk/nationen/1849/')
     titles_1849 = parse_eb_html(raw_1849)
-
     titles = titles_nationen + titles_1849
     append_titles(titles, filepath)
 
